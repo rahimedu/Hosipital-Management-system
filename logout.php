c00395b7e58b7a66ee53267161231a1be763963e
rahimedu <edwin.duche82@gmail.com>
2023-04-08 02:49:22 +0000

    Destroy session

--- logout.php
+++ logout.php
@@ -1,4 +1,11 @@
-eta tags -->
+<?php
+session_start();
+session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
