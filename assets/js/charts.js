125642d902698c51b01637626f0f8ee469f21e5f
rahimedu <edwin.duche82@gmail.com>
2023-04-08 02:35:22 +0000

    Change

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -375,7 +375,19 @@ var Charts = function() {"use strict";
               pointHighlightStroke: 'rgba(220,220,220,1)',
               data: [65, 59, 90, 81, 56, 55, 40]
           },
-  
+          {
+              label: 'My Second dataset',
+              fillColor: 'rgba(151,187,205,0.2)',
+              strokeColor: 'rgba(151,187,205,1)',
+              pointColor: 'rgba(151,187,205,1)',
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(151,187,205,1)',
+              data: [28, 48, 40, 19, 96, 27, 100]
+          }
+        ]
+    };
+
     // Chart.js Options
    var options = {
 
